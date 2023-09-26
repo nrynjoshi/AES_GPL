2387995686f7cc65b8fbf2242846181e7ad5c1c8
nrynjoshi <narayan@uslegalpro.com>
2023-09-26 12:01:54 +0100

    UI work starting

---
+++ src/main/java/com/example/gplapplication/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.gplapplication;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onValidateButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
