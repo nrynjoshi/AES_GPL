39d4966c42399a3b30583358193a9a4c7dc3e203
nrynjoshi <narayan@uslegalpro.com>
2023-10-04 20:44:52 +0100

    command mismatch bug fixed

--- src/main/java/com/example/gplapplication/service/TriangleShape.java
+++ src/main/java/com/example/gplapplication/service/TriangleShape.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class TriangleShape  extends DrawShape{
 
-    public final static String COMMAND= CommandEnum.RECTANGLE.getCommand();//,<hypotenuse>
+    public final static String COMMAND= CommandEnum.TRIANGLE.getCommand();//,<hypotenuse>
 
     public TriangleShape(CanvasUtil canvasUtil){
         super(canvasUtil);
@@ -24,9 +24,6 @@ public class TriangleShape  extends DrawShape{
 
         Util.validateCommand(command, this.COMMAND);
         List<String> params = Util.getAllParameterFromCommand(command);
-
-        System.out.println("Triangle shape draw area.");
-        // Below lines are for shaping Triangle
 
         double moveX = canvasUtil.getMoveX();
         double moveY = canvasUtil.getMoveY();
