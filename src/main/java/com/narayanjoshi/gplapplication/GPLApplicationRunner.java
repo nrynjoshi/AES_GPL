2855ffc0de2ecb4f33c7a43dbcb518286d1daa3d
nrynjoshi <narayan@uslegalpro.com>
2023-10-07 12:59:42 +0100

    some code level documentation

--- src/main/java/com/narayanjoshi/gplapplication/GPLApplicationRunner.java
+++ src/main/java/com/narayanjoshi/gplapplication/GPLApplicationRunner.java
@@ -2,16 +2,21 @@ package com.narayanjoshi.gplapplication;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
+/**
+ * This class is a main class to run this Graphical Programming Language
+ * We have all desktop frame configuration to view for our GPL.
+ * For other view part you will look at  hello-view.fxml file for more details of UI.
+ */
 public class GPLApplicationRunner extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GPLApplicationRunner.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        Scene scene = new Scene((Parent) fxmlLoader.load(), 1000, 600);
         stage.setTitle("Graphical Programming Language");
         stage.setScene(scene);
         stage.setResizable(false);
