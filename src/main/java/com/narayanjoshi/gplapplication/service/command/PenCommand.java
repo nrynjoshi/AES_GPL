e42f91f6642154a6cf9131f1cf0da429e465294c
nrynjoshi <narayan@uslegalpro.com>
2023-10-07 10:03:37 +0100

    pen command enable for wide range of color and handle exception

--- src/main/java/com/narayanjoshi/gplapplication/service/command/PenCommand.java
+++ src/main/java/com/narayanjoshi/gplapplication/service/command/PenCommand.java
@@ -10,18 +10,17 @@ import java.util.List;
 
 public class PenCommand extends RootCommand {
 
-    public static final List<Color> pen_colors= Arrays.asList(Color.RED, Color.BLUE, Color.BLACK, Color.YELLOW, Color.GREEN, Color.GRAY);
-
-
     @Override
     public void validate(String command) {
         Util.validateCommand(command, this.command, this.param);
         String color = Util.getAllParameterFromCommand(command).get(0);
-        Color penColor = Color.valueOf(color);
-        boolean isPenColorContains = pen_colors.contains(penColor);
-        if(!isPenColorContains){
+        try{
+            Color.valueOf(color);
+        }catch (IllegalArgumentException e){
             throw new CommandNotFound(String.format("'%s' pen parameter color is not valid one. Please check docs",command), -1);
         }
+
+
     }
 
     @Override
