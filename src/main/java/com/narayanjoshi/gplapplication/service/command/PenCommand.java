d822a415400ab9485265c692d05eccee992df87d
nrynjoshi <narayan@uslegalpro.com>
2023-11-06 08:15:08 +0000

    pen command comment updated

--- src/main/java/com/narayanjoshi/gplapplication/service/command/PenCommand.java
+++ src/main/java/com/narayanjoshi/gplapplication/service/command/PenCommand.java
@@ -19,8 +19,8 @@ public class PenCommand extends RootCommand {
 
     /**
      * {@inheritDoc}
-     * this will validate the color param passed by user.
-     * @throws CommandNotFound if param color is not defined or valid.
+     * this will validate the colour param passed by user.
+     * @throws CommandNotFound if param colour is not defined or valid.
      */
     @Override
     public void validate(String command) {
@@ -37,6 +37,9 @@ public class PenCommand extends RootCommand {
 
     /**
      * {@inheritDoc}
+     * This method will update the pen colour for drawing purpose and be default it will set set as black.
+     * This will read colour name from parameter of that particular command and changed the pen colour with that one.
+     * @see Color for more information about pen colour acceptance
      */
     @Override
     public void draw(String command) {
