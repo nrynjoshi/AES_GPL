eeba8b2e1a6c3e37d11ad07b536a32b2f9fce089
nrynjoshi <narayan@uslegalpro.com>
2023-11-20 15:30:56 +0000

    variable command work in progress

--- src/main/java/com/narayanjoshi/gplapplication/service/command/programming/VariableCommand.java
+++ src/main/java/com/narayanjoshi/gplapplication/service/command/programming/VariableCommand.java
@@ -1,10 +1,18 @@
 package com.narayanjoshi.gplapplication.service.command.programming;
 
 import com.narayanjoshi.gplapplication.exception.CommandNotFoundException;
+import com.narayanjoshi.gplapplication.util.CanvasUtil;
+import com.narayanjoshi.gplapplication.util.Util;
 
 import java.util.Map;
 
 public class VariableCommand extends ProgrammingRootCommand {
+
+
+    @Override
+    public void validate() {
+
+    }
 
     /**
      * {@inheritDoc}
@@ -15,14 +23,40 @@ public class VariableCommand extends ProgrammingRootCommand {
     public void execute() {
         String userParam = paramList.get(0);
         String[] split = userParam.split("=");
-        String variableName = split[0];
-        String variableValue = split[1];
-        if(canvasUtil.getVariableAndValues().containsKey(variableValue)){
-            throw new CommandNotFoundException(variableName+" has defined already.", -1);
-        }
-        //ignore this as it is comment no process
-        canvasUtil.getVariableAndValues().put(variableName, variableValue);
+        String resultVariableName = split[0];
+        String expression = split[1];
 
+        //ignore this as it is comment no process
+        canvasUtil.getVariableAndValues().put(resultVariableName, String.valueOf(arithmeticOperation(expression)));
+
+    }
+
+    private double arithmeticOperation(String expression) {
+        double result;
+        if (Util.containIgnoreCase(expression, "+")) {
+            String[] splitVariableValue = expression.split("[+]");
+            int operand1 = Util.getOperandValue(canvasUtil, splitVariableValue[0]);
+            int operand2 = Util.getOperandValue(canvasUtil, splitVariableValue[1]);
+            result = operand1 + operand2;
+        } else if (Util.containIgnoreCase(expression, "-")) {
+            String[] splitVariableValue = expression.split("-");
+            int operand1 = Util.getOperandValue(canvasUtil, splitVariableValue[0]);
+            int operand2 = Util.getOperandValue(canvasUtil, splitVariableValue[1]);
+            result = operand1 - operand2;
+        } else if (Util.containIgnoreCase(expression, "/")) {
+            String[] splitVariableValue = expression.split("/");
+            int operand1 = Util.getOperandValue(canvasUtil, splitVariableValue[0]);
+            int operand2 = Util.getOperandValue(canvasUtil, splitVariableValue[1]);
+            result = (double) operand1 / operand2;
+        } else if (Util.containIgnoreCase(expression, "*")) {
+            String[] splitVariableValue = expression.split("[*]");
+            int operand1 = Util.getOperandValue(canvasUtil, splitVariableValue[0]);
+            int operand2 = Util.getOperandValue(canvasUtil, splitVariableValue[1]);
+            result = operand1 * operand2;
+        }  else {
+            result = Util.getOperandValue(canvasUtil, expression);
+        }
+        return result;
     }
 
 }
