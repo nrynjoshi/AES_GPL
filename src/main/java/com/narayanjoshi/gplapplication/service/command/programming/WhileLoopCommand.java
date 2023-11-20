77e2b96668214c1c4754ab8f5f5de461de821353
nrynjoshi <narayan@uslegalpro.com>
2023-11-20 16:18:40 +0000

    while command simple execution done, but nested work left

---
+++ src/main/java/com/narayanjoshi/gplapplication/service/command/programming/WhileLoopCommand.java
@@ -0,0 +1,51 @@
+package com.narayanjoshi.gplapplication.service.command.programming;
+
+import com.narayanjoshi.gplapplication.service.CommandParser;
+import com.narayanjoshi.gplapplication.util.CanvasUtil;
+import com.narayanjoshi.gplapplication.util.Util;
+
+public class WhileLoopCommand   extends ProgrammingRootCommand {
+
+
+    @Override
+    public void validate() {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     * This method will save a command to a file path given by user in command param.
+     */
+    @Override
+    public void execute() {
+        int currentProgramExecutionIndex = canvasUtil.getCurrentProgramExecutionIndex();
+        currentProgramExecutionIndex = loopCommandProcess(currentProgramExecutionIndex);
+        canvasUtil.setCurrentProgramExecutionIndex(currentProgramExecutionIndex);
+    }
+
+    private int loopCommandProcess(int currentExecutionIndex) {
+        String[] commandLineByLineArray = canvasUtil.getCommandLineByLineArray();
+        String chunkCommand = commandLineByLineArray[currentExecutionIndex];
+        String[] loopSplit = chunkCommand.split(" ", 2);
+        String conditionPart = loopSplit[1];
+
+        int loopStatementStartIndex = currentExecutionIndex+1;
+        int loopStatementProcessingIndex = currentExecutionIndex+1;
+        while (evalCondition(conditionPart, canvasUtil)) {
+            String chunkCommandNext = commandLineByLineArray[loopStatementProcessingIndex];
+
+            if (chunkCommandNext.contains("endwhile")) {
+                loopStatementProcessingIndex = loopStatementStartIndex;
+            }else if (chunkCommandNext.contains("while")) {
+                return loopCommandProcess(loopStatementProcessingIndex);
+            }else if(Util.isNotEmpty(chunkCommandNext)){
+                CommandParser commandParser = new CommandParser(canvasUtil.getCanvasId(), null, chunkCommandNext);
+                commandParser.processTheGivenInstruction(chunkCommandNext, canvasUtil, true);
+                loopStatementProcessingIndex++;
+            }
+        }
+        return loopStatementProcessingIndex+1;
+    }
+
+
+}
