bb03efc3f46cf9e0010ae9af40e6831e796be5e5
NARAYAN\geeva <geevan.josh@gmail.com>
2023-12-30 21:10:36 +0000

    variable command validation part completed

--- src/main/java/com/narayanjoshi/gplapplication/service/command/programming/VariableCommand.java
+++ src/main/java/com/narayanjoshi/gplapplication/service/command/programming/VariableCommand.java
@@ -1,6 +1,7 @@
 package com.narayanjoshi.gplapplication.service.command.programming;
 
 import com.narayanjoshi.gplapplication.exception.CommandNotFoundException;
+import com.narayanjoshi.gplapplication.exception.CommandProcessingException;
 import com.narayanjoshi.gplapplication.util.CanvasUtil;
 import com.narayanjoshi.gplapplication.util.Util;
 
@@ -8,9 +9,39 @@ import java.util.Map;
 
 public class VariableCommand extends ProgrammingRootCommand {
 
-
+    /**
+     * {@inheritDoc}
+     * This method will validate a user input command.
+     */
     @Override
     public void validate() {
+        if(paramList.isEmpty()){
+            throw new CommandNotFoundException("Variables command is not valid. ie var a=5. ", -1);
+        }
+        String userParam = paramList.get(0);
+
+        if(!userParam.contains("=")){
+            throw new CommandNotFoundException("Variables command is not valid. '=' operator is missing.", -1);
+        }
+
+        String[] split = userParam.split("=");
+        String resultVariableName = split[0].trim();
+        if(split.length != 2 || resultVariableName.isEmpty()){
+            throw new CommandNotFoundException("Variables command is not valid.'=' operator left hand side variable name is missing.", -1);
+        }
+
+        String resultVariableVal = split[1].trim();
+
+        if(split.length != 2 || resultVariableVal.isEmpty()){
+            throw new CommandNotFoundException("Variables right hand side portion is blank.'=' operator right hand side variable value is missing.", -1);
+        }
+        String expression = split[1].trim();
+
+        try {
+            arithmeticOperation(expression);
+        }catch (Exception x){
+            throw new CommandNotFoundException("The Expression is not valid.", -1);
+        }
 
     }
 
