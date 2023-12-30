9428662d4fa363b8dab6f0cfe3a7b46779dc0f2f
NARAYAN\geeva <geevan.josh@gmail.com>
2023-12-30 14:20:04 +0000

    if statement test case

---
+++ src/test/java/com/narayanjoshi/gplapplication/IfStatementCommandTest.java
@@ -0,0 +1,67 @@
+package com.narayanjoshi.gplapplication;
+
+import com.narayanjoshi.gplapplication.service.CommandParser;
+import com.narayanjoshi.gplapplication.service.RootCommandIfc;
+import com.narayanjoshi.gplapplication.service.command.CommandEnum;
+import com.narayanjoshi.gplapplication.service.command.draw.MoveToCommand;
+import com.narayanjoshi.gplapplication.service.command.programming.IFStatementCommand;
+import com.narayanjoshi.gplapplication.util.CanvasUtil;
+import com.narayanjoshi.gplapplication.util.Util;
+import javafx.scene.canvas.Canvas;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IfStatementCommandTest {
+
+
+    @Test
+    public void ifStatementWithTrueCondition() {
+        String command =
+                "clear\n" +
+                        "   var a=5\n" +
+                        "   if a<50\n" + //this statement will be reverse for two different test case
+                        "       circle a\n" +
+                        "       var a = a+ 80\n" +
+                        "   endif\n" +
+                        "   circle a";
+
+        CanvasUtil canvasUtil = drawMock(command);
+
+        Assertions.assertEquals(0, canvasUtil.getMoveX(), "move first param not set properly.");
+        Assertions.assertEquals(0, canvasUtil.getMoveY(), "move second param not set properly.");
+
+        Assertions.assertEquals("85", canvasUtil.getVariableAndValues().get("a"), "a values changed to 85 i.e initial value changed.");
+
+    }
+
+
+    @Test
+    public void ifStatementWithFalseCondition() {
+        String command =
+                "clear\n" +
+                        "   var a=5\n" +
+                        "   if a>50\n" +
+                        "       circle a\n" +
+                        "       var a = a+ 80\n" +
+                        "   endif\n" +
+                        "   circle a";
+
+        CanvasUtil canvasUtil = drawMock(command);
+
+        Assertions.assertEquals(0, canvasUtil.getMoveX(), "move first param not set properly.");
+        Assertions.assertEquals(0, canvasUtil.getMoveY(), "move second param not set properly.");
+
+        Assertions.assertEquals("5", canvasUtil.getVariableAndValues().get("a"), "a values does not changed here because if condition false.");
+
+
+    }
+
+
+
+    private CanvasUtil drawMock(String command){
+        CanvasUtil canvasUtil = new CanvasUtil(new Canvas());
+        CommandParser commandParser = new CommandParser(canvasUtil.getCanvasId(), null, command);
+        commandParser.processTheGivenInstruction(false, null, command, canvasUtil, false);
+        return canvasUtil;
+    }
+}
